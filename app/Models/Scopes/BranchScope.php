a7239ff27f57ba7d4204398b49d8ed9dbfeaf74a
yovafebrian <yova.febrian512@gmail.com>
2026-03-29 23:37:50 +0700

    fix BranchScope, redis

--- app/Models/Scopes/BranchScope.php
+++ app/Models/Scopes/BranchScope.php
@@ -6,17 +6,34 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
 
+
+use Illuminate\Support\Facades\Auth;
+
 class BranchScope implements Scope
 {
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder<\Illuminate\Database\Eloquent\Model>  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
     public function apply(Builder $builder, Model $model): void
     {
-        $user = auth()->user();
+        /** @var \App\Models\User|\App\Models\Customer|null $user */
+        $user = Auth::user();
 
         if (! $user) {
             return;
         }
 
         if ($user->hasRole('super_admin')) {
+            return;
+        }
+
+        // Pastikan model user memiliki method branches()
+        // karena jika login sebagai Customer, method ini tidak ada.
+        if (! method_exists($user, 'branches')) {
             return;
         }
 
